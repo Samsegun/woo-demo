96fd6204e547eb708b14a1df100bd452846fec9b
Samsegun <oyebadesamsegun@gmail.com>
2023-10-17 09:10:47 +0100

    wave fixes

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const menuBtn = document.getElementById("menu-icon");
 const navWrapper = document.getElementById("page-nav");
 const nav = document.getElementById("nav");
 const menu = document.querySelector("nav .main-nav");
+const modalLink = document.getElementById("modal-link");
+const backdrop = document.querySelector(".backdrop");
+const signinModal = document.querySelector(".popup-modal");
 
 window.onscroll = function () {
     if (
@@ -18,4 +21,14 @@ window.onscroll = function () {
 
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("translate-menu");
+});
+
+modalLink.addEventListener("click", () => {
+    signinModal.classList.add("open");
+    backdrop.classList.add("open");
+});
+
+backdrop.addEventListener("click", () => {
+    signinModal.classList.remove("open");
+    backdrop.classList.remove("open");
 });
